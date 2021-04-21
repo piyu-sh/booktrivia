03b88e6aa6ad61e600099c286493e3ae2f9d5931
pi <piyush_g@outlook.com>
2021-04-21 23:37:22 +0530

    use asyncio event loop"

--- src/proxy-server/handler.py
+++ src/proxy-server/handler.py
@@ -1,16 +1,33 @@
 import json
 from aiohttp import web
 from proxies import fetch_proxies
+import asyncio
 
+proxiesObj = ""
 
-async def hello(event, context):
+# async def hello(event, context):
+#     result = ''
+#     try:
+#         ifNumInParams = event['pathParameters'] is not None and event['pathParameters']['num'] is not None
+#         numProxies = int(event['pathParameters']['num']) if ifNumInParams is True else 1
+#         proxies = await fetch_proxies(numProxies)
+#         result = web.json_response(proxies)
+#     except Exception as e:
+#         print(e)
+#     return result
+
+async def getProxiesObject(numProxies):
+    global proxiesObj
+    proxiesObj = await fetch_proxies(numProxies)
+
+def hello(event, context):
     result = ''
     try:
         ifNumInParams = event['pathParameters'] is not None and event['pathParameters']['num'] is not None
         numProxies = int(event['pathParameters']['num']) if ifNumInParams is True else 1
-        proxies = await fetch_proxies(numProxies)
-        result = web.json_response(proxies)
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(getProxiesObject(numProxies))
+        result =  web.json_response(proxiesObj)
     except Exception as e:
         print(e)
     return result
-
