727b52cea3828973584e4b52798d6ff813d603a2
pi <piyush_g@outlook.com>
2021-04-23 23:31:18 +0530

    change name of function

--- src/proxy-server/handler.py
+++ src/proxy-server/handler.py
@@ -5,22 +5,11 @@ import asyncio
 
 proxiesObj = ""
 
-# async def hello(event, context):
-#     result = ''
-#     try:
-#         ifNumInParams = event['pathParameters'] is not None and event['pathParameters']['num'] is not None
-#         numProxies = int(event['pathParameters']['num']) if ifNumInParams is True else 1
-#         proxies = await fetch_proxies(numProxies)
-#         result = web.json_response(proxies)
-#     except Exception as e:
-#         print(e)
-#     return result
-
 async def getProxiesObject(numProxies):
     global proxiesObj
     proxiesObj = await fetch_proxies(numProxies)
 
-def hello(event, context):
+def getOwnProxies(event, context):
     result = ''
     try:
         ifNumInParams = event['pathParameters'] is not None and event['pathParameters']['num'] is not None
