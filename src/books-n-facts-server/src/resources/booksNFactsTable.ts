2555f1e48ae445801bb64c27b30b678828f7b939
pi <piyush_g@outlook.com>
2021-05-11 23:53:16 +0530

    update table schema

--- src/books-n-facts-server/src/resources/booksNFactsTable.ts
+++ src/books-n-facts-server/src/resources/booksNFactsTable.ts
@@ -1,25 +1,37 @@
 import type { AWS } from '@serverless/typescript';
 
-export const booksNFactsTable: AWS['resources']['Resources'][keyof AWS['resources']['Resources']]= {
+export const booksNFactsTable: AWS['resources']['Resources'][keyof AWS['resources']['Resources']] = {
     "Type": "AWS::DynamoDB::Table",
     // "DeletionPolicy": "Retain", // Keeps around the DynamoDB resource when we redeploy/destroy
     "Properties": {
-       "TableName": "booksNFactsTable",
-       "AttributeDefinitions": [
-          {
-             "AttributeName": "id",
-             "AttributeType": "S"
-          }
-       ],
-       "KeySchema": [
-          {
-             "AttributeName": "id",
-             "KeyType": "HASH"
-          }
-       ],
-       "ProvisionedThroughput": {
-          "ReadCapacityUnits": 4,
-          "WriteCapacityUnits": 4
-       }
+        "TableName": "booksNFactsTable",
+        "AttributeDefinitions": [
+            {
+                "AttributeName": "id",
+                "AttributeType": "S"
+            },
+            {
+                "AttributeName": "title",
+                "AttributeType": "S"
+            },
+            {
+                "AttributeName": "info",
+                "AttributeType": "S"
+            },
+            {
+                "AttributeName": "facts",
+                "AttributeType": "S"
+            }
+        ],
+        "KeySchema": [
+            {
+                "AttributeName": "id",
+                "KeyType": "HASH"
+            }
+        ],
+        "ProvisionedThroughput": {
+            "ReadCapacityUnits": 4,
+            "WriteCapacityUnits": 4
+        }
     }
- }
+}
