74ecc6cf495917af51df37ed465d0c164b91b5a8
pi <piyush_g@outlook.com>
2021-05-14 16:13:10 +0530

    enforce post request and schema

--- src/books-n-facts-server/src/functions/getBooksNFacts/index.ts
+++ src/books-n-facts-server/src/functions/getBooksNFacts/index.ts
@@ -1,19 +1,20 @@
 // import schema from './schema';
 import { handlerPath } from '@libs/handlerResolver';
 import type { AWS } from '@serverless/typescript';
+import schema from './schema';
 
 export default {
   handler: `${handlerPath(__dirname)}/handler.main`,
   events: [
     {
       http: {
-        method: 'get',
+        method: 'post',
         path: 'getBooksNFacts',
-        // request: {
-        //   schema: {
-        //     'application/json': schema
-        //   },
-        // }
+        request: {
+          schema: {
+            'application/json': schema
+          },
+        }
       }
     }
   ],
