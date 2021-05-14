299d3957b6541875f921f55ac988aa221cf3e4c3
pi <piyush_g@outlook.com>
2021-05-14 16:00:11 +0530

    fetch books from keywords api

--- src/books-n-facts-server/config/index.ts
+++ src/books-n-facts-server/config/index.ts
@@ -1,5 +1,5 @@
 export default { 
     flipkartAPI: {
-        keyword: 'https://affiliate-api.flipkart.net/affiliate/1.0/search.json'
+        keywordAPI: 'https://affiliate-api.flipkart.net/affiliate/1.0/search.json'
     }
 }
--- src/books-n-facts-server/src/functions/getBooksNFacts/handler.ts
+++ src/books-n-facts-server/src/functions/getBooksNFacts/handler.ts
@@ -1,19 +1,37 @@
 import 'source-map-support/register';
-
+import fetch from 'node-fetch';
+import { stringify} from 'querystring';
 // import { DynamoDB } from 'aws-sdk'
 
 import type { ValidatedEventAPIGatewayProxyEvent } from '@libs/apiGateway';
 import { formatJSONResponse } from '@libs/apiGateway';
 import { middyfy } from '@libs/lambda';
-
 import schema from './schema';
-
+import config from '@config/index'
 const hello: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (event) => {
 
+  const { flipkartAPI: {keywordAPI}} = config;
+  const { flipkartApiKey, flipkartAffiliateId } = process.env;
   // get keyword from query
-    // const { query, count } = event.body;
-    // flipkartApiKey: ${process.env.flipkartApiKey} flipkartAffiliateId: ${process.env.flipkartAffiliateId}
+  const { query, count } = event.body;
+
   // get books from flipkart
+
+  const queryParams = {
+    query: query,
+    resultCount: count
+  }
+  const queryUrl = `${keywordAPI}?${stringify(queryParams)}`;
+  console.log("🚀 ~ file: handler.ts ~ line 25 ~ consthello:ValidatedEventAPIGatewayProxyEvent<typeofschema>= ~ queryUrl", queryUrl)
+  
+  const response = await fetch(queryUrl, {
+    headers: {
+      'Fk-Affiliate-Id': flipkartAffiliateId,
+      'Fk-Affiliate-Token': flipkartApiKey
+    }
+  });
+  const flipkartBooks = await response.json();
+  console.log("🚀 ~ file: handler.ts ~ line 34 ~ consthello:ValidatedEventAPIGatewayProxyEvent<typeofschema>= ~ flipkartBooks", flipkartBooks)
   // save books data in dynamodb if not present
   // return books data to ui
   return formatJSONResponse({
