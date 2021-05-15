652a38f8046003243eb462dfb2f7c19359f41aa9
pi <piyush_g@outlook.com>
2021-05-15 16:19:34 +0530

    dynamoddb tables only have attributes which are in keys

--- src/books-n-facts-server/src/resources/booksNFactsTable.ts
+++ src/books-n-facts-server/src/resources/booksNFactsTable.ts
@@ -3,23 +3,12 @@ import type { AWS } from '@serverless/typescript';
 export const booksNFactsTable: AWS['resources']['Resources'][keyof AWS['resources']['Resources']] = {
     "Type": "AWS::DynamoDB::Table",
     "DeletionPolicy": "Retain", // Keeps around the DynamoDB resource when we redeploy/destroy
+    // "DeletionPolicy": "Delete",
     "Properties": {
         "TableName": "booksNFactsTable",
         "AttributeDefinitions": [
             {
                 "AttributeName": "id",
-                "AttributeType": "S"
-            },
-            {
-                "AttributeName": "title",
-                "AttributeType": "S"
-            },
-            {
-                "AttributeName": "info",
-                "AttributeType": "S"
-            },
-            {
-                "AttributeName": "facts",
                 "AttributeType": "S"
             }
         ],
